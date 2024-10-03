7db0ad97fe1f71f6ecb1c0b398d16cdb4e46649e
celineee <cwon0113@student.Monash.edu>
2024-10-03 16:11:54 +0800

    updated files and html
    updated files and html

---
+++ vega_lite_vis.js
@@ -0,0 +1,10 @@
+var vg_1 = "js/Map1.json";
+
+
+
+vegaEmbed("#choropleth_map", vg_1).then(function(result) {
+// Access the Vega view instance
+
+//(https://vega.github.io/vega/docs/api/view/) as result.view
+
+}).catch(console.error);
