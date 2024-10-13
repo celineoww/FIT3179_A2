882b62b7f91533595f48a00bde99d95e37f86937
celineee <cwon0113@student.Monash.edu>
2024-10-13 15:00:07 +0800

    updated the final implementation
    updated the final implementation

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -3,6 +3,7 @@ var vg_2 = "js/BumpChart.json";
 var vg_3 = "js/BarChart.json";
 var vg_4 = "js/StackedAreaChart.json";
 var vg_5 = "js/WaffleChart.json"
+var vg_6 = "js/BarChart2.json"
 
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
   
@@ -24,5 +25,9 @@ vegaEmbed("#area_chart", vg_4).then(function(result) {
 }).catch(console.error);
 
 vegaEmbed("#waffle_chart", vg_5).then(function(result) {
+  
+}).catch(console.error);
+
+vegaEmbed("#bar2_chart", vg_6).then(function(result) {
   
 }).catch(console.error);
